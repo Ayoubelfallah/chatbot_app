bf104a3233e25dbc0d9b4359d151e6026a71878f
Ayoub EL Fallah <95918228+Ayoubelfallah@users.noreply.github.com>
2022-05-13 03:24:52 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from unicodedata import name
+from flask import flask, jsonify,request
+import time 
+
+app=flask(__name__)
+@app.route("/bot",method=["POST"])
+
+
+def response(): 
+     query = dict(request.form)['query']
+     result = query +""+time.ctime()
+     return jsonify({"response":result})
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",)
